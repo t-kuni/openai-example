3625c8d3a50f42071ba7b8370c1fa975e90a202d
t-kuni <t.kuni.dev@gmail.com>
2023-10-16 17:34:51 +0900

    update

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt = open("prompt.txt", "r").read()
+
+print("[Q] " + prompt)
+
+response = openai.ChatCompletion.create(
+    # model="gpt-4",
+    model="gpt-4-0314",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+msg = response.choices[0].message['content']
+
+with open("output.txt", "w") as f:
+    f.write(msg)
+
+print("[A] " + msg)
